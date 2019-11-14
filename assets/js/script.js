2683a866e832cadc7e20ae554bc84b4b2e62cd0a
Ronnie <ronniebrown@gmail.com>
2019-11-14 20:50:59 +0000

    Updates

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+{
+	var introHeader = document.querySelector('h1.welcomeText');
+
+	var hourNow = new Date().getHours();
+	if (hourNow >= 12) {
+		introHeader.innerHTML = 'Feasgar math!';
+	} else {
+		introHeader.innerHTML = 'Madainn mhath!';
+	}
+}

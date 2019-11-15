b29d1a0aaf329ddb50da623fc903955060211069
Ronnie Brown <ronnie.brown@hilton.com>
2019-11-15 11:21:14 +0000

    Adding webpack

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 {
-	var introHeader = document.querySelector('h1.welcomeText');
+    var introHeader = document.querySelector('h1.welcomeText');
 
-	var hourNow = new Date().getHours();
-	if (hourNow >= 12) {
-		introHeader.innerHTML = 'Feasgar math!';
-	} else {
-		introHeader.innerHTML = 'Madainn mhath!';
-	}
+    var hourNow = new Date().getHours();
+    if (hourNow >= 12) {
+        introHeader.innerHTML = 'H1Feasgar math!';
+    } else {
+        introHeader.innerHTML = 'H1 Madainn mhath!';
+    }
 }

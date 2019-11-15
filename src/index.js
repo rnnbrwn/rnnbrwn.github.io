e74def04d69ce6ef91ceb1ed619b58231fd7d345
Ronnie <ronniebrown@gmail.com>
2019-11-15 18:02:44 +0000

    More webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+
 {
     let introHeader = document.querySelector('h1.welcomeText');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
-    //...
-    watch: true,
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js'
-    }
+  // watch: true,
+  entry: './src/index.js',
+  entry: './src/index.html',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };

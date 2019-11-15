a3ea48422cf2664a903b04a08933b6ec00748fe5
Ronnie Brown <ronnie.brown@hilton.com>
2019-11-15 14:03:45 +0000

    More webpack ini

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+{
+    let introHeader = document.querySelector('h1.welcomeText');
+
+    var hourNow = new Date().getHours();
+    if (hourNow >= 12) {
+        introHeader.innerHTML = 'Feasgar math!';
+    } else {
+        introHeader.innerHTML = 'Madainn mhath!';
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //...
+    watch: true,
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js'
+    }
+};

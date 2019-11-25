dc57dfbc474e5ef1f10a8f623ceea8990fe98a51
Ronnie <ronniebrown@gmail.com>
2019-11-25 20:29:30 +0000

    gulp init

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
 // compile scss into css
 function style() {
 	return (
 		gulp
 			// 1. Where is my scss file?
-			.src('./assets/**/*.scss')
+			.src('./scss/**/*.scss')
 
 			// 2. Pass file through sass compiler
 			.pipe(sass())
 
 			// 3. Where do I save the compiled css?
-			.pipe(gulp.dest('./assets/css'))
+			.pipe(gulp.dest('./css'))
 	);
 }
 

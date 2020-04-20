a9e14e03ed98195cb4bcf9602c280b386a5bdbea
Ronnie Brown <ronnie.brown@hilton.com>
2020-04-20 21:25:43 +0100

    robots 23-24

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function addRobot() {
+function changeRobot() {
   //change this if you add a new robot svg
-  var howManyRobots = 23;
+  var howManyRobots = 24;
 
   var numRobot = Math.floor(Math.random() * howManyRobots + 1);
   document.getElementById('robot-img').src =
     '../media/robot0' + numRobot + '.svg';
 }
-addRobot();
+changeRobot();

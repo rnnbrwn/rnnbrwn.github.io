02c888730ca4dd44ed10756fa39efc4b7e54fa5b
Ronnie Brown <ronnie.brown@hilton.com>
2020-04-20 18:05:21 +0100

    added a third robot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function addRobot() {
-  var numRobot = Math.floor(Math.random() * 2 + 1);
+  var numRobot = Math.floor(Math.random() * 3 + 1);
   document.getElementById('robot-img').src =
     '../media/robot0' + numRobot + '.svg';
 }
